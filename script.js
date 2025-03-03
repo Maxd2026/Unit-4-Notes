98b0b146c87932fa5aeb311869fa8afc647d8d1e
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-03-03 18:40:09 +0000

    1

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+/* This is a MULTI-LINE comment 
+The code below is an example STATEMNT 
+(instruction/command)
+console.log is a FUNCTION that displays info in the console (output) area
+STATEMENTS end with a SEMICOLON (;)
+*/
+console.log("Today is Monday");
+
+// This is a SINGLE-LINE comment
+
+// DECLARE a new VARIABLE
+let pokemon;
+
+// ASSIGN a VALUE to the variable
+// its like putting an iten in the box
+pokemon = "Pikachu";
