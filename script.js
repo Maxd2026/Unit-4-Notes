ec2786eb4fec41838e06df224b69c749e3736149
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-03-10 17:40:45 +0000

    4

--- script.js
+++ script.js
@@ -49,3 +49,30 @@ score = score * (-3);
 console.log(score);
 score = score / 5;
 console.log(score);
+
+// Can combine operators, evaluation follows PEMDAS
+score = (score * 3 / 7 - 4 + (-6)) ** 2 ;
+console.log(score);
+
+// STRING CONCATENATION (merging with the PLUS SIGN)
+// String LITERALS are indicated with "QUOTES"
+let greeting = "Hello " + "Friend" ;
+console.log(greeting) ;
+
+// Can combine String Literals with VARIABLES
+let username = "MAX_D";
+console.log("Welcome, " + username);
+console.log("Welcome,  \n " + username + "1");
+
+// Numbers get converted to Strings if combined
+console.log( 4 + 5 ); // 9 (did addition)
+console.log( "4" + 5) // 45 (concatenate, not math)
+console.log( 4 + '5'); // same as above
+
+// SHORTCUT OPERATORS (Increment/Decrement)
+let counter = 0;
+counter = counter + 1; // LONG WAY
+counter++; // SHORTCUT
+console.log("Count is " + counter);
+counter--;
+console.log("Count is " + counter);
